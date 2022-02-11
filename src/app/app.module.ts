d971bf3554777cf405bbd01ca8e58c78bf8ce845
tan095297 <tan.yout001@gmail.com>
2022-02-11 13:17:13 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/shared/navigation/navigation.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { ProductComponent } from './components/pages/product/product.component';
+import { NewProductComponent } from './components/pages/new-product/new-product.component';
+import { EditProductComponent } from './components/pages/edit-product/edit-product.component';
+import { ProductDetailComponent } from './components/pages/product-detail/product-detail.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './components/pages/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    MainComponent,
+    ProductComponent,
+    NewProductComponent,
+    EditProductComponent,
+    ProductDetailComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
